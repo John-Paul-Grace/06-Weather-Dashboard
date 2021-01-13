30bb0d659837e2f07ed79af394cb84b94289e11d
JP-Grace <gracejohnpaul200@gmail.com>
2021-01-13 17:43:18 -0500

    added click listener #city-submit button; also added comments

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,12 +4,26 @@ $(document).ready(function() {
     // addCity() accepts a string as a parameter. It makes a new entry in #city-list for the parameter.
     function addCity(newCity) {
 
+        // creates a new element that has the Bootstrap "list-group-item" class
         var newCityEl = $("<li>").addClass("list-group-item");
 
+        // makes the parameter string the text of the new element
         newCityEl.text(newCity);
 
+        // appends the new element to the city list
         $("#city-list").append(newCityEl);
     }
 
-    addCity("Atlanta");
+    // click listener for the #city-submit button
+    $("#city-submit").on("click", function(event) {
+
+        // prevents refreshing on click
+        event.preventDefault();
+
+        // sets the value of #city-search to a variable
+        var userInput = $("#city-search").val();
+
+        // runs the addCity() method with the above variable as input
+        addCity(userInput);
+    });
 });
