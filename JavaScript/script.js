7051a485617a810ba507b4d8b7c4da4de23bbb0d
JP-Grace <gracejohnpaul200@gmail.com>
2021-01-13 17:36:20 -0500

    implemented addCity()

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,15 @@
 // jQuery ready method
 $(document).ready(function() {
-    
+
+    // addCity() accepts a string as a parameter. It makes a new entry in #city-list for the parameter.
+    function addCity(newCity) {
+
+        var newCityEl = $("<li>").addClass("list-group-item");
+
+        newCityEl.text(newCity);
+
+        $("#city-list").append(newCityEl);
+    }
+
+    addCity("Atlanta");
 });
